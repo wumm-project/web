e0b0c55818172bb87e85b3fc18768d59c5b553d1
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2019-07-09 11:04:36 +0200

    small extension, not yet completed

---
+++ glossary.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'lib/EasyRdf.php';
+require_once 'helper.php';
+
+function theGlossary($input) 
+{
+    EasyRdf_Namespace::set('od', 'http://opendiscovery.org/rdf/Model#');
+    EasyRdf_Namespace::set('foaf', 'http://xmlns.com/foaf/0.1/');
+    EasyRdf_Namespace::set('owl', 'http://www.w3.org/2002/07/owl#');
+    $graph = new EasyRdf_Graph('http://opendiscovery.org/rdf/Glossary/');
+    $graph->parseFile($input);
+    $a=array();
+    $res = $graph->allOfType('foaf:Person');
+    foreach ($res as $autor) {
+        $b=array();
+        foreach ($autor->all("foaf:name") as $e) {
+            $b[]='<span itemprop="name" class="foaf:name">'
+                .$e->getValue().'</span>';
+        }
+        foreach ($autor->all("foaf:affil") as $e) {
+            $b[]='<span itemprop="affiliation" class="foaf:affil">'
+                .$e->getValue().'</span>';
+        }
+        foreach ($autor->all("foaf:homepage") as $e) {
+            $b[]=createLink($e,$e);
+        }
+        $a[$autor->getUri()]=
+            '<div itemscope itemtype="http://schema.org/Person" class="creator">'
+            .join('<br/>',$b).'</p></div>';
+    }
+    ksort($a);
+    $out='<h3>People in the TRIZ Social Network</h3>
+<div class="people">
+'.join("\n", $a).'
+</div> <!-- end class people -->';
+    return htmlEnv($out);
+}
+
+function main() {
+    $input=$_GET["people"];
+    return theGlossary($input);
+    
+}
+
+echo main();
+
+?>
