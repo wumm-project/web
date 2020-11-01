cb08a5e7e847be932d12e5994d0ee0f85ed02e0d
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2020-11-01 20:06:15 +0100

    loaddata.pl and OntoCards added

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# A small demo site for some applications within the WUMM Project.
+# WUMM Project - Demonstration Site
+
+## The code
+
+The repository contains the code of a small demo site for some applications
+within the WUMM Project.
 
 It uses lightweight PHP scripts based on the Bootstrap Framework and the
 EasyRdf PHP Library.
+
+At the moment this website is running at <http://wumm.uni-leipzig.de/>.
+
+## Additional services
+
+Part of the data is uploaded in a [Virtuoso](https://virtuoso.openlinksw.com/)
+based RDF store that operates a __SPARQL Endpoint__ at
+<http://wumm.uni-leipzig.de:8891/sparql>.
+
+The _services_ directory contains Perl code for maintenance of that endpoint.  
---
+++ services/loaddata.pl
@@ -0,0 +1,45 @@
+##################################
+#
+# author: graebe
+# lastModified: 2020-11-01
+
+# Changes:
+
+# purpose: load data into the local Virtuoso store
+# usage: perl loaddata.pl | isql-vt 1112 dba <YourSecretPassword>
+
+my $RDFData="/local/home/wumm/web/rdf";
+#my $RDFData="/home/graebe/git/WUMM/web/rdf";
+# print cleardata();
+print loaddata();
+
+## end main ## 
+
+sub cleardata { # nicht aktuell
+  return <<EOT;
+sparql clear graph <http://opendiscovery.org/rdf/People/> ;
+EOT
+}
+
+sub loaddata {
+  my $out;
+  $out.=createLoadCommand("http://opendiscovery.org/rdf/People/","People.rdf");
+  $out.=createLoadCommand("http://opendiscovery.org/rdf/MATRIZ-Certificates/","MATRIZ-Certificates.rdf"); 
+  $out.=createLoadCommand("http://opendiscovery.org/rdf/Thesaurus/","Thesaurus.rdf");
+  $out.=createLoadCommand("http://opendiscovery.org/rdf/VDI-Glossary/","VDI-Glossary.rdf");
+  $out.=createLoadCommand("http://opendiscovery.org/rdf/OntoCards/","OntoCards.rdf");
+  return $out;
+}
+
+sub createLoadCommand {
+  my ($graph,$file)=@_;
+  return<<EOT;
+sparql clear graph <$graph> ;
+sparql create silent graph <$graph> ; 
+DB.DBA.TTLP_MT (file_to_string_output('$RDFData/$file'),'$graph'); 
+EOT
+}
+
+
+__END__
+
