14686888bc932955023659be98f3bd8dedae4f8e
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2022-06-16 08:29:51 +0200

    update

--- bmp.php
+++ bmp.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * User: Hans-Gert Gräbe
- * last update: 2022-05-20
+ * last update: 2022-06-16
  */
 
 require 'vendor/autoload.php';
@@ -16,9 +16,9 @@ function theBusinessModelPatterns()
 PREFIX skos: <http://www.w3.org/2004/02/skos/core#> 
 PREFIX od: <http://opendiscovery.org/rdf/Model#>
 
-describe ?a
+describe ?a ?b
 from <http://opendiscovery.org/rdf/BusinessModelPatterns/>
-where { ?a a od:BusinessModelPattern . }';
+where { ?a a od:BusinessModelPattern . ?b a od:BMP-Dimension .}';
 
     try {
         $graph = $sparql->query($query);
@@ -29,12 +29,19 @@ where { ?a a od:BusinessModelPattern . }';
     foreach($graph->allOfType('od:BusinessModelPattern') as $bmp) {
         $comment=$bmp->get("rdfs:comment");
         $note=$bmp->all("skos:note");
+        $dim=array();
+        foreach ($bmp->all("od:hasDimension") as $v) {
+            $dim[]=$v->get("skos:prefLabel");
+        }
         $uri=str_replace('http://opendiscovery.org/rdf/BusinessModelPattern/'
                          ,'',$bmp->getURI());
         $out="<h3>".$uri."</h3>";
         $out.="<p>".showLanguage($bmp->all("skos:prefLabel"),"<br/>")."</p>";
         if (!empty($note)) {
             $out.="<h4>Note</h4><p>".showLanguage($note,"<br/>")."</p>";
+        }
+        if (!empty($dim)) {
+            $out.="<strong>Affected BM Components</strong>: ".join(", ",$dim)."</p>";
         }
         if (!empty($comment)) {
             $out.="<h4>Comment</h4><p>".$comment."</p>";
