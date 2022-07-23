0ba4e8ed085551ff5bfab57701d3677161bc8cfd
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2022-07-23 11:09:49 +0200

    update

--- loaddata.pl
+++ loaddata.pl
@@ -8,8 +8,8 @@
 # purpose: load data into the local Virtuoso store
 # usage: perl loaddata.pl | isql-vt 1112 dba <YourSecretPassword>
 
-#my $RDFData="/local/home/wumm/web/rdf";
-my $RDFData="/home/graebe/git/WUMM/web/rdf";
+my $RDFData="/local/home/wumm/web/rdf";
+#my $RDFData="/home/graebe/git/WUMM/web/rdf";
 # print cleardata();
 print loaddata();
 
